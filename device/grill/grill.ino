13097345001784ac96c2a0b283ae8499f2c9e08f
John Jones <johnjones@toughbook.local.johnjonesfour.com>
2021-06-17 12:40:16 -0400

    working temp sensor

--- device/grill/grill.ino
+++ device/grill/grill.ino
@@ -7,9 +7,16 @@
  
 // What pin to connect the sensor to
 #define THERMISTORPIN A0 
+
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 100000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25  
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
  
 void setup(void) {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
  
 void loop(void) {
@@ -21,10 +28,23 @@ void loop(void) {
   Serial.println(reading);
  
   // convert the value to resistance
-  reading = (1023 / reading)  - 1;     // (1023/ADC - 1) 
-  reading = SERIESRESISTOR / reading;  // 10K / (1023/ADC - 1)
+  reading = (4095 / reading)  - 1;     // (1023/ADC - 1) 
+  reading = SERIESRESISTOR / reading;  // 100K / (1023/ADC - 1)
   Serial.print("Thermistor resistance "); 
   Serial.println(reading);
+
+  float steinhart;
+  steinhart = reading / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert absolute temp to C
+  
+  Serial.print("Temperature "); 
+  Serial.print(steinhart);
+  Serial.println(" *C");
+
  
   delay(1000);
 }
